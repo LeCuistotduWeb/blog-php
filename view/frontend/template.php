14f47610d2407da97eb5f00489eca997a9a929fd
LeCuistotduWeb <gaetanboyron@gmail.com>
2018-02-15 11:46:38 +0100

    ajout du plugin  WYSWIYG tinyMce

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -14,6 +14,10 @@
         <hr>
         <p>&copy; Company 2017-2018</p>
       </footer>
+
+      <!-- script tiny mce -->
+      <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+      <script>tinymce.init({ selector:'.tinyMce' });</script>
     <!-- script -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
